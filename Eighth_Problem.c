817342e426f724b058189768083bfd274fe6c5a2
Nabaraj Rai <115961746+samee-rae@users.noreply.github.com>
2023-02-15 19:43:40 +0545

    Add files via upload

---
+++ Eighth_Problem.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int n,o,h;
+	printf("\nEnter any decimal number: ");
+	scanf("%d",&n);
+	printf("\nOctal of %d is %o",n,n);
+	printf("\nHexadecimal of %d is %X",n,n);
+	getch();
+}
